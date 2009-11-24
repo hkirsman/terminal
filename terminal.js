30511936951c95d95432279b5e308f4abdd122da
Balazs Dianiska <balazs@longlake.co.uk>
2009-11-24 23:06:41 +0000

    Drupal 6 port #635458 by RobLoach with some minor modifications.

--- terminal.js
+++ terminal.js
@@ -1,64 +1,90 @@
 // $Id$
 
+/**
+ * The base DrupalTerminal namespace.
+ */
+DrupalTerminal = {
+  visible: 0
+};
+
 if (Drupal.jsEnabled) {
   $(document).ready(function() {
-      // Attach the terminal to the bottom, hidden by default
-      $('html').append('<div id="terminal-container" style="display: none"></div>').append('<img id="terminal-corner" src="images/bar.png" />');
+    // Attach the terminal to the bottom, hidden by default
+    $('html').append('<div id="terminal-container" style="display: none"></div>').append('<img id="terminal-corner" src="images/bar.png" />');
 
-      // Corner recover icon
-      $('#terminal-corner').text('^---');
+    // Corner recover icon
+    $('#terminal-corner').text('^---');
 
-      var user = Drupal.settings['user'];
-      var host = Drupal.settings['user'];
-      var sitename = Drupal.settings['sitename'];
-      var welcome_message = 'Welcome to ' + sitename + '!';
-      var prompt = user + '@' + host + ' >';
+    var user = Drupal.settings['terminal']['user'];
+    var host = Drupal.settings['terminal']['server'];
+    var sitename = Drupal.settings['terminal']['sitename'];
+    var welcome_message = 'Welcome to ' + sitename + '!';
+    var prompt = user + '@' + host + ' >';
 
-      $('#terminal-container').terminal('index.php?q=terminal/input', {custom_prompt : prompt, hello_message : welcome_message});
+    $('#terminal-container').terminal(Drupal.settings.basePath + '?q=terminal/input', {custom_prompt : prompt, hello_message : welcome_message});
 
-      $.get('index.php?q=terminal/state', function(state) {
-        if ('visible' == state) {
-          $('#terminal-container').show();
+    // See whether or not we should display the terminal on page load.
+    DrupalTerminal.visible = jQuery.cookie ? $.cookie('DrupalTerminal') : 0;
+    if (DrupalTerminal.visible == 1) {
+      $('#terminal-container').show();
+      $('#terminal-corner').hide();
+    }
+    else {
+      $('#terminal-container').hide();
+      $('#terminal-corner').show();
+    }
+
+    // Register the terminal events.
+    $('#terminal-container').dblclick( function() {
+      DrupalTerminal.close();
+    });
+    $('#terminal-corner').click( function() {
+      DrupalTerminal.open();
+    });
+    if (window.addEventListener) {
+      window.addEventListener("keydown", function(e) {
+        if (e.keyCode == 192) {
+          DrupalTerminal.toggle();
         }
-        else {
-          $('#terminal-corner').show();
-        }
-      });
-
-
-      // Attach click events
-      DrupalTerminal.toggle();
+      }, true);
+    }
   });
 }
 
-DrupalTerminal = new Object;
-
-DrupalTerminal.toggle = function () {
-  $('#terminal-container').dblclick( function() {
-      $(this).toggle("slow", function() {
-        $('#terminal-corner').show();
-        DrupalTerminal.set('hidden');
-        });
-      });
-  $('#terminal-corner').click( function() {
-      $('#terminal-container').toggle("slow", function() {
-        $('#terminal-corner').hide();
-        DrupalTerminal.set('visible');
-        });
-      });
+/**
+ * Closes or displays the terminal depending on its current visibility.
+ */
+DrupalTerminal.toggle = function() {
+  if (DrupalTerminal.visible) {
+    return DrupalTerminal.close();
+  }
+  else {
+    return DrupalTerminal.open();
+  }
 }
 
 /**
- * Set the terminal state via callback
+ * Opens the terminal.
  */
-DrupalTerminal.set = function(state) {
-  $.get('index.php?q=terminal/state/' + state);
-}
-
-/**
- * Fetch the terminal state.
- */
-DrupalTerminal.get = function() {
-  $.get('index.php?q=terminal/state', function(state) {
+DrupalTerminal.open = function() {
+  $('#terminal-container').slideDown("slow", function() {
+    $('#terminal-corner').hide();
   });
-}
+  if (jQuery.cookie) {
+    $.cookie('DrupalTerminal', 1);
+  }
+  return DrupalTerminal.visible = 1;
+};
+
+/**
+ * Closes the terminal.
+ */
+DrupalTerminal.close = function() {
+  $('#terminal-container').slideUp("slow", function() {
+    $('#terminal-corner').show();
+  });
+  if (jQuery.cookie) {
+    $.cookie('DrupalTerminal', 0);
+  }
+  return DrupalTerminal.visible = 0;
+};
