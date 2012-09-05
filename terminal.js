84e97878d5e67f85ad62b5c4bc4fc1ed27457357
Hannes Kirsman <hannes.kirsman@eesti.ee>
2012-09-05 16:03:01 +0300

    From 6.x to 7.x

--- terminal.js
+++ terminal.js
@@ -2,20 +2,20 @@
 /**
  * The base DrupalTerminal namespace.
  */
-DrupalTerminal = {
-  visible: 0
-};
+(function ($) {
+  DrupalTerminal = {
+    visible: 0
+  };
 
-if (Drupal.jsEnabled) {
   $(document).ready(function() {
-    $('html').append(Drupal.settings['terminal']['terminal']);
+    $('body').append(Drupal.settings['terminal']['terminal']);
     // Corner recover icon
     var recover_text = '^---';
 
     var welcome_message = Drupal.settings['terminal']['welcome_message'];
     var prompt = Drupal.settings['terminal']['prompt'];
 
-    $('#terminal-container .body').terminal(Drupal.settings.basePath + '?q=terminal/input', {custom_prompt : prompt, hello_message : welcome_message});
+    $('#terminal-container .body').terminal(Drupal.settings.basePath + 'terminal/input', {custom_prompt : prompt, hello_message : welcome_message});
 
     // See whether or not we should display the terminal on page load.
     DrupalTerminal.visible = jQuery.cookie ? $.cookie('DrupalTerminal') : 0;
@@ -37,53 +37,55 @@ if (Drupal.jsEnabled) {
     // We are also listening to the keyboard.
     if (window.addEventListener) {
       window.addEventListener("keydown", function(e) {
+        // Key besides backspace
         if (e.keyCode == 192) {
           DrupalTerminal.toggle();
         }
       }, true);
     }
   });
-}
 
-/**
- * Closes or displays the terminal depending on its current visibility.
- */
-DrupalTerminal.toggle = function() {
-  if (DrupalTerminal.visible) {
-    return DrupalTerminal.close();
-  }
-  else {
-    return DrupalTerminal.open();
-  }
-}
 
-/**
- * Opens the terminal.
- */
-DrupalTerminal.open = function() {
-  $('#terminal-container .body').slideDown("slow", function() {
-      $(this).show();
-      $('#terminal-container').height('200px');
-      $('#terminal-container .title').text('');
-  });
-  if (jQuery.cookie) {
-    $.cookie('DrupalTerminal', 1);
+  /**
+   * Closes or displays the terminal depending on its current visibility.
+   */
+  DrupalTerminal.toggle = function() {
+    if (DrupalTerminal.visible) {
+      return DrupalTerminal.close();
+    }
+    else {
+      return DrupalTerminal.open();
+    }
   }
-  return DrupalTerminal.visible = 1;
-};
 
-/**
- * Closes the terminal.
- */
-DrupalTerminal.close = function() {
-  $('#terminal-container .body').slideUp("slow", function() {
-      $('#terminal-container').height('20px');
-      $('#terminal-container .title').text('^---');
-      $(this).hide();
-  });
-  if (jQuery.cookie) {
-    $.cookie('DrupalTerminal', 0);
-  }
-  return DrupalTerminal.visible = 0;
-};
+  /**
+   * Opens the terminal.
+   */
+  DrupalTerminal.open = function() {
+    $('#terminal-container .body').slideDown("slow", function() {
+        $(this).show();
+        $('#terminal-container').height('200px');
+        $('#terminal-container .title').text('');
+    });
+    if (jQuery.cookie) {
+      $.cookie('DrupalTerminal', 1);
+    }
+    return DrupalTerminal.visible = 1;
+  };
 
+  /**
+   * Closes the terminal.
+   */
+  DrupalTerminal.close = function() {
+    $('#terminal-container .body').slideUp("slow", function() {
+        $('#terminal-container').height('20px');
+        $('#terminal-container .title').text('^---');
+        $(this).hide();
+    });
+    if (jQuery.cookie) {
+      $.cookie('DrupalTerminal', 0);
+    }
+    return DrupalTerminal.visible = 0;
+  };
+
+})(jQuery);
